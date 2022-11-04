e8a291680e37a748ba4af968bd243d12939de1e7
darkmal <vagnerwillians@hotmail.com>
2022-11-03 22:12:30 -0300

    letzte Änderung

--- src/components/Hintergrund.js
+++ src/components/Hintergrund.js
@@ -15,7 +15,7 @@ const Hintergrund = () => {
 				<h1>ACQUIRE THE UNCANNY ABILITY THAT WILL MAKE YOU AN AWARD-WINNING FICTIONAL WRITER</h1>
 				{/* <h1>Acquire The Uncanny Ability <br/>That Will Make You An Award-Winning Fictional Writer</h1> */}
 				<h2>You will learn special skills that easily generate interesting ideas <br/>And you will be prepared to never suffer from writer's block</h2>
-				<p>Get this unique opportunity to find out how the most prominet fictional writers master the craft<br/> that always engage their readers</p>
+				<p>So get this unique opportunity to find out how the most prominet fictional writers master the craft<br/> that always engage their readers</p>
 			</div>
 
 			<div className="myFormContainer">
