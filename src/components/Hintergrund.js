4dff7c90704a7d12b0e127374996b71a2b195a6d
darkmal <vagnerwillians@hotmail.com>
2022-11-03 21:40:25 -0300

    03-11 21:40hs

--- src/components/Hintergrund.js
+++ src/components/Hintergrund.js
@@ -3,9 +3,9 @@ import React from "react";
 import './Hintergrund.css';
 
 const Hintergrund = () => {
-	const greet = () => {
-		alert("Hallo");
-	};
+	// const greet = () => {
+	// 	alert("Hallo");
+	// };
 
 	return (
 		<div className="background">
@@ -15,7 +15,7 @@ const Hintergrund = () => {
 				<h1>ACQUIRE THE UNCANNY ABILITY THAT WILL MAKE YOU AN AWARD-WINNING FICTIONAL WRITER</h1>
 				{/* <h1>Acquire The Uncanny Ability <br/>That Will Make You An Award-Winning Fictional Writer</h1> */}
 				<h2>You will learn special skills that easily generate interesting ideas <br/>And you will be prepared to never suffer from writer's block</h2>
-				<p>Get this unique opportunity to find out how the most prominet fictional writers master the craft that always engage their readers</p>
+				<p>Get this unique opportunity to find out how the most prominet fictional writers master the craft<br/> that always engage their readers</p>
 			</div>
 
 			<div className="myFormContainer">
@@ -28,14 +28,14 @@ const Hintergrund = () => {
 						<input type="text" name="email" placeholder="Email"/>
 					</div>
 
-					<div onClick={greet}>
+			
+					<div>  {/*  onClick={greet} */}
 						<input type="submit" id="button" value="YES, I WANT TO BE A WRITER"></input>
 					</div>
 
 				</form>
 			</div>		
 			{/* <div className="clear"></div> */}
-
 		</div>
 	)
 }
