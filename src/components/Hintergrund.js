9fa00e0d149396c2b0a79b20cf22b5ffd152dc72
darkmal <vagnerwillians@hotmail.com>
2022-11-05 10:05:26 -0300

    05-11

--- src/components/Hintergrund.js
+++ src/components/Hintergrund.js
@@ -16,7 +16,7 @@ const Hintergrund = () => {
 				<h1>ACQUIRE THE UNCANNY ABILITY THAT WILL MAKE YOU AN AWARD-WINNING FICTIONAL WRITER</h1>
 				<h2>You will learn special skills that easily generate interesting ideas <br/>and you will be prepared to never suffer from writer's block</h2>
 				<p>So get this unique opportunity to find out how the most prominet fictional writers<br/> master the craft that always engage their readers</p><br/>
-				<p className="offer">Special Offer! <s>200$</s> 60$</p> 
+				<p className="offer">Special Offer! <s>$200</s> $60</p> 
 			</div>
 
 			<div className="myFormContainer">
